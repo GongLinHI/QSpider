e7cfc41136d68bcd38e00dad80642f3048834953
GongLinHI <hi_lig@163.com>
2024-05-21 18:08:43 +0800

    [修复]-Demo中的代码

--- demo.py
+++ demo.py
@@ -1,13 +1,3 @@
-# import random
-# from QSpider import QSpider
-#
-# if __name__ == '__main__':
-#     id = 'rXz5Xu9'
-#     for i in range(1, 100):
-#         spider = QSpider(id)
-#         t = random.randrange(5, 15)
-#         time.sleep(t)
-#         spider.submit(r'1$1}2$测试【1】' + f'{i}')
 from QSpider import QSpider
 
 if __name__ == '__main__':
